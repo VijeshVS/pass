9dc68ee708ca0678f02a91d15072e87efd1bc966
Vijesh Shetty <vijeshsshetty@gmail.com>
2025-05-15 22:52:28 +0530

    fixed date in event

--- app/components/editComp/EventForm.tsx
+++ app/components/editComp/EventForm.tsx
@@ -201,8 +201,8 @@ const EventForm: React.FC<EventFormProps> = ({ event, onSave }) => {
             <input
               id="date"
               name="date"
-              // type="date"
-              value={formatDateForInput(formData.date)}
+              type="text"
+              value={formData.date}
               onChange={handleChange}
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
             />
